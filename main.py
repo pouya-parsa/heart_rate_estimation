0ec50b6b9f9608191ca22c26cb027df30f842ac6
Pouya <mail.pouyaparsa@gmail.com>
2021-08-31 04:39:33 +0430

    Adding GUI Elements

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivymd.uix.label import MDLabel
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 
+from kivymd.font_definitions import theme_font_styles
+
 import cv2
 import dlib
 from imutils import face_utils
@@ -31,26 +33,50 @@ class mainApp(MDApp):
         self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
         # fa = face_utils.FaceAligner(predictor, desiredFaceWidth=256)
 
-        layout = BoxLayout()
+        container_layout = BoxLayout(orientation="vertical")
+
         
-        self.img = Image()
+        
+        header_layout = BoxLayout(size_hint=(1, 0.25), padding = (80, 25))
+        header_layout.add_widget(
+            Image(
+                source="hr_image.png", 
+                size_hint=(None, None), 
+                width=75, 
+                height=75,
+                pos=(100, 1),))
+        
+        header_layout.add_widget(
+            MDLabel(
+                text="Heart Rate via Webcam", 
+                halign="center", 
+                size_hint=(0.9, 1),
+                font_style=theme_font_styles[2]))
+
+        body_layout = BoxLayout(spacing=20, padding=50)
+
+        self.img = Image(size_hint=(0.75, 1))
         
         self.label = MDLabel(
             text="Heart Rate: (Processing)",
             halign="center",
+            size_hint=(0.25, 1),
+            font_style=theme_font_styles[4]
         )
 
-        layout.add_widget(self.img)
+        body_layout.add_widget(self.img)
         
-        layout.add_widget(self.label)
+        body_layout.add_widget(self.label)
 
-        time.sleep(1) 
         self.capture = cv2.VideoCapture(0)
         
         
         Clock.schedule_interval(self.update, 1.0/38.0)
 
-        return layout
+        container_layout.add_widget(header_layout)
+        container_layout.add_widget(body_layout)
+
+        return container_layout
 
     
     def update(self, dt):
@@ -65,7 +91,7 @@ class mainApp(MDApp):
         buf = frame.tostring()
 
 
-        self.label.text = "Heart Rate : (" + str(round(np.mean(np.array(self.bpms)), 2)) + "!)"
+        self.label.text = "Heart Rate : (" + str(round(np.mean(np.array(self.bpms)), 1)) + " BPM)"
 
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
 
